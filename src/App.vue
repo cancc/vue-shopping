e95725df4237c782551ab30be806cca5ab164b37
cancc <1051024728@qq.com>
2019-04-05 03:05:14 +0800

    添加返回按钮

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="app">
     <!-- 头部区域 -->
-    <mt-header fixed title="vue 项目"></mt-header>
+    <mt-header fixed title="vue 项目">
+      <span slot="left" @click="back" v-show="backFlag">
+        <mt-button icon="back">返回</mt-button>
+      </span>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
     <!-- 中间路由区域 -->
     <transition>
       <router-view></router-view>
@@ -12,28 +17,61 @@
 </template>
 
 <script>
-import FooterGuide from './components/FooterGuide/FooterGuide'
-  export default {
-
-    components: {
-      FooterGuide
+import FooterGuide from "./components/FooterGuide/FooterGuide";
+export default {
+  data() {
+    return {
+      backFlag: false
+    };
+  },
+  created() {
+    this.refresh()
+  },
+  methods: {
+    back() {
+      this.$router.go(-1);
+    },
+    refresh() {
+      if(this.$route.path == "/home") {
+        this.backFlag = false
+      }else {
+        this.backFlag = true
+      }
     }
+  },
+  watch: {
+    "$route.path": function(newVal) {
+      if (newVal == "/home") {
+        this.backFlag = false
+      }else {
+        this.backFlag = true
+      }
+    }
+  },
+  components: {
+    FooterGuide
   }
+};
 </script>
 
 <style lang="stylus" rel="${stylesheet/stylus}">
-.app
-  padding-top 40px
-  padding-bottom 50px
-  overflow-x hidden
-  .v-enter,
-    opacity 0
-    transform translateX(100%)
-  .v-leave-to
-    opacity 0
-    transform translateX(-100%)
-    position absolute
-  .v-enter-active,
-  .v-leave-active
-    transition  all 0.5s ease
+.app {
+  padding-top: 40px;
+  padding-bottom: 50px;
+  overflow-x: hidden;
+
+  .v-enter, opacity 0 {
+    transform: translateX(100%);
+  }
+
+  .v-leave-to {
+    opacity: 0;
+    transform: translateX(-100%);
+    position: absolute;
+  }
+
+  .v-enter-active, .v-leave-active {
+    transition: all 0.5s ease;
+  }
+}
 </style>
